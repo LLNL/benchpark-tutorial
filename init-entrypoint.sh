59d7a466c5fbf98adcbef21ef5a6b4714d8d1fb4
Ian Lumsden <lumsden.ian@gmail.com>
2025-06-11 15:53:02 -0700

    Adds licensing info to prepare for the move to the LLNL org

--- init-entrypoint.sh
+++ init-entrypoint.sh
@@ -1,3 +1,8 @@
+# Copyright 2025 Lawrence Livermore National Security, LLC and other
+# Benchpark developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 #!/usr/bin/env sh 
 
 # NOTE: this script runs as root
