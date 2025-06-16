8fe4fd3bd7246a94d151e91a49250ac211b5ef7a
Ian Lumsden <lumsden.ian@gmail.com>
2025-06-16 16:18:10 -0700

    Migrates initial repo structure and Dockerfiles into the repo (#1)
    * Sets up the images and adds basic documentation
    
    * Adds licensing info to prepare for the move to the LLNL org
    
    * Restructures the repo similar to how Flux does it in preparation for the move to the LLNL org
    
    * Adds some stub directories and a README describing my intent with the current layout
    
    * Defines GitHub Actions CI for testing the builds of the Docker images and pushing the images to GHCR
    
    * Adds a simple PR template for the repo
    
    * Changes README for HPDC 2025 to RST
    
    * Updates the "tag" field in github_ci_matrix.json for the move to the LLNL org
    
    * Fixes some tags in the Dockerfiles
    
    * Updates image names
    
    * Fixes GHCR image names to be lowercase

---
+++ 2025-HPDC/docker/spawn-entrypoint.sh
@@ -0,0 +1,8 @@
+# Copyright 2025 Lawrence Livermore National Security, LLC and other
+# Benchpark developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+#!/usr/bin/env bash
+
+/opt/global_py_venv/bin/jupyterhub-singleuser
