b3edbe0b17ce4e4796d55d7c9eb9fd25c4ab8ce2
Ian Lumsden <lumsden.ian@gmail.com>
2025-06-17 09:04:12 -0700

    Entrypoint shebang fix (#2)
    * Fixes shebang location for init-entrypoint.sh
    
    * Fixes shebang location for spawn-entrypoint.sh

--- 2025-HPDC/docker/init-entrypoint.sh
+++ 2025-HPDC/docker/init-entrypoint.sh
@@ -1,9 +1,8 @@
+#!/usr/bin/env sh 
 # Copyright 2025 Lawrence Livermore National Security, LLC and other
 # Benchpark developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: Apache-2.0
-
-#!/usr/bin/env sh 
 
 # NOTE: this script runs as root
 
