76af8c3e426f520ba1768f56f599b94aeb27faf9
Ian Lumsden <lumsden.ian@gmail.com>
2025-07-07 10:40:56 -0700

    Updates the Docker images so that users run under Flux (#5)
    * Launches Jupyter under Flux to allow us to emulate multi-node
    
    * Moves some bash setup to the Caliper Dockerfile and removes the install of OpenMPI since the Flux based image already installs MPICH

--- 2025-HPDC/docker/spawn-local-entrypoint.sh
+++ 2025-HPDC/docker/spawn-local-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-/opt/global_py_venv/bin/jupyter-lab --ip=0.0.0.0
+num_cores_per_node=2
+total_num_cores=$(nproc --all)
+num_brokers=$(( $total_num_cores / $num_cores_per_node ))
+
+/usr/bin/mpiexec.hydra -n $num_brokers -bind-to core:$num_cores_per_node /usr/bin/flux start /opt/global_py_venv/bin/jupyter-lab --ip=0.0.0.0
